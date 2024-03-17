1f5e49f841fa610d74ae0813c9c7ee0692bc6be8
Alex Vaiman <vaiman.software@gmail.com>
2024-03-18 00:17:43 +0200

    fix url path

--- app/src/main/java/com/example/easyfood/data/retrofit/FoodApi.kt
+++ app/src/main/java/com/example/easyfood/data/retrofit/FoodApi.kt
@@ -10,15 +10,15 @@ interface FoodApi {
     @GET("categories.php")
     fun getCategories(): Call<CategoryResponse>
 
-    @GET("filter.php?")
-    fun getMealsByCategory(@Query("i") category:String):Call<MealsResponse>
+    @GET("filter.php")
+    fun getMealsByCategory(@Query("c") category:String):Call<MealsResponse>
 
     @GET ("random.php")
     fun getRandomMeal():Call<RandomMealResponse>
 
-    @GET("lookup.php?")
+    @GET("lookup.php")
     fun getMealById(@Query("i") id:String):Call<RandomMealResponse>
 
-    @GET("search.php?")
+    @GET("search.php")
     fun getMealByName(@Query("s") s:String):Call<RandomMealResponse>
 }
